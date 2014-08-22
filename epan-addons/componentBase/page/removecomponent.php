511617ea0527a637af376b7b40ff87ec76db11d1
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-08-22 10:05:50 +0530

    BUG FIXED: Remove Component from marketplace removes epan-components folder

--- epan-addons/componentBase/page/removecomponent.php
+++ epan-addons/componentBase/page/removecomponent.php
@@ -1,6 +1,7 @@
 <?php
 class page_componentBase_page_removecomponent extends Page {
 	function remove(){
+
 		$this->api->stickyGET('component_id');
 		$v=$this->add('View_Error')->set('Are You Sure ?');
 		$btn=$this->add('Button')->set('Confirm');
@@ -8,7 +9,8 @@ class page_componentBase_page_removecomponent extends Page {
 		// Controller EPAN to check if system or not
 		// If not from system pages show error in controller to 
 
-		if($btn->isClicked()){			
+		if($btn->isClicked()){		
+				
 			$installed=$this->api->current_website
 					->ref('InstalledComponents')
 					->addCondition('component_id',$_GET['component_id'])
@@ -20,17 +22,16 @@ class page_componentBase_page_removecomponent extends Page {
 			$marketplace=$this->add('Model_MarketPlace');
 			$marketplace->load($_GET['component_id']);
 			
-			// Remove from MarketPlace Row
-			$marketplace->delete();
-
 			// Run UnInstall.sql if exists
 			if(file_exists($file = getcwd().DS.'epan-components'.DS.$marketplace['namespace'].DS.'uninstall.sql')){
 				$sql = file_get_contents($file);
 				$this->api->db->dsql($this->api->db->dsql()->expr($sql))->execute();
 			}
-			 
-			//and remove Folder
-			destroy_dir(dirname($file));
+
+			// Remove from MarketPlace Row
+			$marketplace->delete();
+
+			// Folder removed in beforeDelete Hook
 
 			$this->js()->univ()->successMessage('Done')->execute();
 		}
