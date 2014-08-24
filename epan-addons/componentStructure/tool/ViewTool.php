9823357061e19843bbba2df44a9896c0dd884c93
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-08-24 14:13:59 +0530

    html_attribute class variable added in component structre

--- epan-addons/componentStructure/tool/ViewTool.php
+++ epan-addons/componentStructure/tool/ViewTool.php
@@ -3,6 +3,7 @@
 namespace {namespace};
 
 class {ToolName} extends \componentBase\View_Component{
+	public $html_attributes=array(); // ONLY Available in server side components
 	function init(){
 		parent::init();
 	}
