b753299fea10a458e26a8a41f618278cc328db6f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-09-19 10:30:14 +0530

    Template sticked for epan_id

--- lib/Model/EpanTemplates.php
+++ lib/Model/EpanTemplates.php
@@ -6,6 +6,7 @@ class Model_EpanTemplates extends Model_Table {
 		parent::init();
 
 		$this->hasOne('Epan','epan_id');
+		$this->addCondition('epan_id',$this->api->current_website->id);
 		$this->addField('name')->defaultValue('Default');
 		$this->addField('content')->type('text')->defaultValue('<div component_namespace="baseElements" component_type="TemplateContentRegion" class="epan-sortable-component epan-component  ui-sortable" style="" contenteditable="false">{{Content}}</div>');
 		$this->addField('css')->type('text');
