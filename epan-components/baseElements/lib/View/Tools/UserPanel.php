3f843a8179abd3105c42cf17c92304ae7385be03
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-09-23 12:07:08 +0530

    User Panel Files added

---
+++ epan-components/baseElements/lib/View/Tools/UserPanel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace baseElements;
+
+class View_Tools_UserPanel extends \componentBase\View_Component{
+	public $html_attributes=array(); // ONLY Available in server side components
+	
+	function init(){
+		parent::init();
+
+		if(!$this->api->isLoggedIn()){
+			// create login form
+		}else{
+			// create hello user panel
+		}
+
+	}
+
+	// defined in parent class
+	// Template of this tool is view/namespace-ToolName.html
+}
